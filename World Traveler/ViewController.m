311e46d8fa1ff271e7217f23c31269f57f4f0e97
zac1st1k <zacfirst@gmail.com>
2015-01-22 18:56:53 +1100

    Initial Commit

---
+++ World Traveler/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  World Traveler
+//
+//  Created by Zac on 22/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ World Traveler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  World Traveler
+//
+//  Created by Zac on 22/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
